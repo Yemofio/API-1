2d63c90cc7796754ae30ca254ae8b50b0b1bc238
Yemofio <95033608+Yemofio@users.noreply.github.com>
2023-06-27 09:17:57 +0000

    Update search.js

--- search.js
+++ search.js
@@ -1,3 +1,4 @@
+// Import required modules
 const express = require('express');
 const router = express.Router();
 const { Validator } = require('jsonschema');
@@ -10,17 +11,20 @@ function validateSchema(data, schema) {
   return v.validate(data, schema);
 }
 
+// Define a route for searching
 router.post('/search', (req, res) => {
   // Validate the request payload against the search schema
   const validationResult = validateSchema(req.body, searchSchema);
   if (!validationResult.valid) {
+    // If the request payload is invalid, return a 400 Bad Request response
     return res.status(400).json({ error: 'Invalid request payload', details: validationResult.errors });
   }
 
+  // Extract the query and filters from the request body
   const { query, filters } = req.body;
 
   // Perform the search operation based on the query and filters
-  // Replace this with implementation using remote database server
+  // Replace this with implementation using a remote database server
 
   // Example response data 
   const results = [
@@ -32,6 +36,7 @@ router.post('/search', (req, res) => {
   // Validate the response payload against the search result schema
   const searchResultValidationResult = validateSchema({ results }, searchResultSchema);
   if (!searchResultValidationResult.valid) {
+    // If the response payload is invalid, return a 500 Internal Server Error response
     return res.status(500).json({ error: 'Internal Server Error' });
   }
 
@@ -39,4 +44,5 @@ router.post('/search', (req, res) => {
   res.json({ results });
 });
 
+// Export the router
 module.exports = router;
